452af4bb0f7cd3fc4efc01d71c33c31aa6e2a9bb
Mahdi <mahdiprog81@gmail.com>
2025-10-29 17:32:59 +0330

    fix:build error

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonProps } from "@heroui/button";
+import { Button, ButtonProps } from "@heroui/react";
 import { JSX } from "react";
 
 const CustomButton = (props: ButtonProps): JSX.Element => {
